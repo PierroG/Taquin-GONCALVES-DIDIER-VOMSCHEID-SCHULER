66f954dd44441f18f0298f0451a4614d66216f1a
admin <admin@192.168.43.94>
2018-11-10 15:10:26 +0100

    Creation connexion à la base

---
+++ src/Jdbc.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Gwendolyn
+ */
+import java.sql.*;
+
+public class Jdbc {
+    
+    private String username = "";
+    private String connectUrl = "";
+    private String mdp = "";
+    private static Connection con = null;
+    
+    //Constructeur privé
+    private Jdbc(){
+      try {
+          con = DriverManager.getConnection(connectUrl, username, mdp);
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+    }
+    
+    //methode qui va nous retourner notre instance et la créer si elle n'existe pas
+    public static Connection getInstance(){
+     if(con == null){
+       new Jdbc();
+       System.out.println("la connexion a la base est cree");
+     }
+     else {
+       System.out.println("la connexion a la base a echouee");
+     }
+     return con;   
+   }
+    
+    //methode qui ferme la connexion a la base si elle a ete auparavant ouverte
+    public static void closeInstance(){
+        if(con != null){
+            try {
+                con.close();
+                System.out.println("connexion a la base close");
+            } catch(Exception e){ 
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    public void inscription(){
+        try {
+        
+        } catch(Exception e){
+            System.out.println("Erreur");
+        }
+    }
+}
